543e32f590ac311b4289822341d50adf54a2a7cd
patrik121212 <119336295+patrik121212@users.noreply.github.com>
2025-12-18 01:27:35 +0800

    Add migration files for personal access tokens, posts, likes, comments, and messages tables

---
+++ database/migrations/2025_12_17_135232_create_posts_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        // membuat tabel posts
+        Schema::create('posts', function (Blueprint $table) {
+            $table->id();
+            $table->integer('user_id');
+            $table->text('content');
+            $table->string('image_url')->nullable();
+            $table->timestamps(0);
+            $table->softDeletes();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('posts');
+    }
+};
---
+++ database/migrations/2025_12_17_135541_create_messages_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        // membuat tabel messages
+        Schema::create('messages', function (Blueprint $table) {
+            $table->id();
+            $table->integer('sender_id');
+            $table->integer('receiver_id');
+            $table->text('message_content');
+            $table->timestamps(0);
+            $table->softDeletes();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('messages');
+    }
+};
