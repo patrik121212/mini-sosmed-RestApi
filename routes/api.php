a6d1324ef3c2275e72f1064d991c25e37eacee3f
patrik121212 <119336295+patrik121212@users.noreply.github.com>
2025-12-26 11:49:22 +0800

    Refactor API routes to enforce JWT middleware for posts, comments, likes, and messages

--- routes/api.php
+++ routes/api.php
@@ -9,20 +9,17 @@ use App\Http\Controllers\CommentsController;
 use App\Http\Controllers\LikesController;
 use App\Http\Controllers\MessageController;
 use App\Http\Controllers\JWTAuthController;
-
-
-Route::get('/user', function (Request $request) {
-    return $request->user();
-})->middleware('auth:sanctum');
+use App\Http\Middleware\JWTMiddleware;
 
 
 Route::prefix('v1')->group(function () {
     // handle user authentication
     Route::post('register', [JWTAuthController::class, 'register']);
+    Route::post('login', [JWTAuthController::class, 'login']);
 
 
     // menghandle posts
-    Route::prefix('posts')->group(function () {
+    Route::middleware(JWTMiddleware::class)->prefix('posts')->group(function () {
         Route::get('/', [PostsController::class, 'index']); //mengambil semua data posts
         Route::post('/', [PostsController::class, 'store']); //menyimpan data post baru
         Route::get('{id}', [PostsController::class, 'show']); //mengambil data post berdasarkan id
@@ -31,18 +28,18 @@ Route::prefix('v1')->group(function () {
 
     });
     // menghandle comments
-    Route::prefix('comments')->group(function () {
+    Route::middleware(JWTMiddleware::class)->prefix('comments')->group(function () {
         Route::post('/', [CommentsControler::class, 'store']);
         Route::delete('{id}', [CommentsControler::class, 'destroy']);
     });
     // menghandle likes
-    Route::prefix('likes')->group(function () {
+    Route::middleware(JWTMiddleware::class)->prefix('likes')->group(function () {
         Route::post('/', [LikesController::class, 'store']); //menyimpan data like baru
         Route::delete('{id}', [LikesController::class, 'destroy']); //menghapus data like berdasarkan id
     });
 
     // menghandle messages
-    Route::prefix('messages')->group(function () {
+    Route::middleware(JWTMiddleware::class)->prefix('messages')->group(function () {
         Route::post('/', [MessagesController::class, 'store']); //menyimpan data message baru
         Route::get('{id}', [MessagesController::class, 'show']); //mengambil atau melihat data message berdasarkan id
         Route::get('/getMessages/{user_id}', [MessagesController::class, 'getMessages']); //menampilkan pesan berdasrakan user_id
